316b82681343e6cb1be0709555445cf11804d67d
Gordon Stangler <gordon.stangler@gmail.com>
2019-11-04 08:00:11 -0500

    Create Kubernetes.sh
    Initial upload, marked as sh, need to fix

---
+++ Kubernetes.sh
@@ -0,0 +1,21 @@
+kubectl get services  #gets all services
+
+kubectl get pods -o widw  #all pods, verbose
+kubectl get pods --all-namespaces  #gets all pods
+
+kubectl get pod myPod -o yaml  #gets pod's YAML
+
+#upgrade node
+def UpGradeTheNode(nameOfNode):
+	#drain in prep
+	kubectl cordon nameOfNode
+	kubectl drain nameOfNode
+	kubectl get pod -o=custom-columns=nameOfNode:.metadata.name,STATUS:.status.pase,NODE:.spec.nameOfNode  #--all-namespaces
+	#partial patch
+	kubectl patch node k8s-node-1 -p '{"spec":{"unscheduable":true}}'
+	#anything else that needs to be done, goes here
+
+	#end by putting node back into place
+	kubectl uncordon nameOfNode
+
+#end of UpGradeTheNode
